3a18f9cb078a4d06d71b5ab0c31f3c3cfb603610
Georg Graf (priv) <georg@graf.priv.at>
2023-05-30 20:07:59 +0200

    small cleanups

--- HTML_CSS_Javascript/Mastermind/mastermindcolors.js
+++ HTML_CSS_Javascript/Mastermind/mastermindcolors.js
@@ -1,4 +1,10 @@
+"use strict"
 class MasterMindColors {
+
+    parent = undefined
+    currentValue = undefined
+    visible = undefined
+
     static rgbValues = [
         "#ff0000",
         "#00ff00",
@@ -7,27 +13,49 @@ class MasterMindColors {
         "#00ffff",
         "#ff00ff"
     ]
+
+    // liefere mir den RGB String für 0 .. 5
     static getRGB(i) {
         return this.rgbValues[i % this.rgbValues.length]
     }
+
     constructor(parent) {  // 
         this.parent = parent
-        this.currentValue = -1
+        this.currentValue = 0
+        this.visible = true
         //this.updateParent()
     }
-    display(bool) {
-        if (bool) {
+
+    setVisible(bool) {
+        this.visible = bool
+        this.updateDisplay()
+    }
+
+    toggleVisibility() {
+        this.visible = !this.visible
+        this.updateDisplay()
+    }
+
+    updateDisplay() {
+        if (this.visible) {
             this.parent.style.backgroundColor = MasterMindColors.getRGB(this.currentValue)
         } else {
             this.parent.style.backgroundColor = "#ddd"
         }
     }
+
     randomize() {
         this.currentValue = Math.round(Math.random() * 6)
-        this.display()
+        this.updateDisplay()
     }
+
     rotate() {
+        if (!this.visible) {
+            console.log("NOT CHANGING AN INVISIBLE CIRCLE")
+            return
+        }
         this.currentValue = (this.currentValue + 1) % 6
-        this.display()
+        this.updateDisplay()
     }
+
 }
--- HTML_CSS_Javascript/Mastermind/script.js
+++ HTML_CSS_Javascript/Mastermind/script.js
@@ -1,3 +1,5 @@
+"use strict"
+
 var obj;
 var masterCode = document.getElementById("masterCode")
 
@@ -14,33 +16,39 @@ function shuffle(row) {
     }
 }
 
+function makeVisible(row, bool) {
+    cl("makeVibile", row, bool)
+    row.visible = bool
+    for (let child of row.children) {
+        if (child.classList.contains("circle")) {
+            child.colorrotator.setVisible(bool)
+        }
+    }
+}
+
+// Hilfsfunktion, damit ich dann mit dem obj in der Konsole herumspielen kann
 function capture(par) {
     cl("capture (stored in obj)", par)
     obj = par
 }
 
-function makeVisible(row, bool) {
-    cl("makeVibile", row, bool)
-    row.visibility = bool
-    for (let child of row.children) {
-        if (child.classList.contains("circle")) {
-            child.colorrotator.display(bool)
-        }
-    }
-}
 
 function initGuesses() {
     // TODO clear guesses and add one empty guess
 }
+
+// onclick des "toggle Visibility" Buttons
 function toggleVisibility(row) {
     cl("togglevisibilty", row)
-    row.visibility = !row.visibility
-    makeVisible(row, row.visibility)
-
+    row.visible = !row.visible
+    makeVisible(row, row.visible)
 }
+
+// alle "circle" child elemente kriegen den colorrotator angehängt
+// die "row" kriegt den boolean "visible"
 function ammendCode(row) {
     cl("ammendCode", row)
-    row.visibility = true
+    row.visible = true
     for (let child of row.children) {
         if (child.classList.contains("circle") && child.colorrotator == undefined) {
             child.colorrotator = new MasterMindColors(child)
@@ -48,12 +56,14 @@ function ammendCode(row) {
     }
 }
 
+// all - purpose console log (alle argumente)
 function cl() {
     for (let i = 0; i < arguments.length; i++) {
         console.log(arguments[i]);
     }
 }
 
+// alles Neu machen
 function newGame() {
     ammendCode(masterCode)
     shuffle(masterCode)
@@ -61,4 +71,5 @@ function newGame() {
     initGuesses()
 }
 
+// beim Laden der Seite
 window.onload = newGame()
