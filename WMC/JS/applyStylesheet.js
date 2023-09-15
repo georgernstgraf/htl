bb6c2d17ece1206f9c0aa9f0a6024a5727490600
Georg Graf (SPG) <grafg@spengergasse.at>
2023-09-15 08:04:10 +0200

    change your theme on the fly ;)

---
+++ WMC/JS/applyStylesheet.js
@@ -0,0 +1,22 @@
+function applyStylesheet(css = 0) {
+    // List of known resources
+    const sheets = [
+        "https://thomasf.github.io/solarized-css/solarized-dark.min.css",
+        "https://thomasf.github.io/solarized-css/solarized-light.min.css",
+        "https://capp.nicepage.com/eef5851c6a60a800bcae9cceeea3f547dc0330ed/main-libs.css",
+    ];
+    // sanitize input
+    css = Math.floor(css);
+    css %= sheets.length;
+    // Remove all stylesheets and styles
+    document
+        .querySelectorAll('link[rel="stylesheet"], style')
+        .forEach((s) => s.remove());
+    // create and apply a new stylesheet
+    const ss = document.createElement("link");
+    ss.rel = "stylesheet";
+    ss.type = "text/css";
+    ss.href = sheets[css];
+    console.info("Applying stylesheet from", css);
+    document.getElementsByTagName("head")[0].appendChild(ss);
+}
