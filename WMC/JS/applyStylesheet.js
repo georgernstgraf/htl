a8cef043215a4afcefa3494619859b3035ff232d
Georg Graf (SPG) <grafg@spengergasse.at>
2023-09-15 08:28:17 +0200

    youtube

--- WMC/JS/applyStylesheet.js
+++ WMC/JS/applyStylesheet.js
@@ -4,14 +4,16 @@ function applyStylesheet(css = 0) {
         "https://thomasf.github.io/solarized-css/solarized-dark.min.css",
         "https://thomasf.github.io/solarized-css/solarized-light.min.css",
         "https://capp.nicepage.com/eef5851c6a60a800bcae9cceeea3f547dc0330ed/main-libs.css",
+        "https://www.youtube.com/s/desktop/a6a78e6c/cssbin/www-main-desktop-home-page-skeleton-2x.css",
     ];
     // sanitize input
     css = Math.floor(css);
     css %= sheets.length;
     // Remove all stylesheets and styles
-    document
-        .querySelectorAll('link[rel="stylesheet"], style')
-        .forEach((s) => s.remove());
+    document.querySelectorAll('link[rel="stylesheet"], style').forEach((s) => {
+        console.log("removing", s.href);
+        s.remove();
+    });
     // create and apply a new stylesheet
     const ss = document.createElement("link");
     ss.rel = "stylesheet";
